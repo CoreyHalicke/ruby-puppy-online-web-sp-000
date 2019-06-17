59678010a5b0ad17cc3b9e51f6e60af2a17ea61b
Corey H <c.l.halicke@gmail.com>
2019-06-17 22:32:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,7 @@ class Dog
 
   def initialize(name)
     @name = name
-    @@all << name
+    @@all << self
   end
 
 
@@ -14,7 +14,7 @@ class Dog
   end
 
   def self.all
-    puts @@all
+    print @@all
   end
 
 
